dea4f782bc231c49cc80ae9bb5089c3011680210
INDRANEEL SRIVASTAVA <indraneel.max@gmail.com>
2017-09-08 00:36:38 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rename_file_sequence',
+    version='1.0.0',
+    packages=['rename_file_sequence'],
+    url='https://github.com/indraneelmax/rename_file_sequence',
+    license='',
+    author='isrivastava',
+    author_email='indraneel.max@gmail.com',
+    description='Identifies file sequence in a dir and renames them in sequentially keeping the same order'
+)
